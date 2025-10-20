8e8f0ecf76596256dcc1b39989679908657723f7
Chu Đức Thắng <chuducthang77@gmail.com>
2025-10-20 15:51:48 -0600

    Adapt the deepsea plotting code to chain mdp plotting

--- chain_mdp_plotting.py
+++ chain_mdp_plotting.py
@@ -22,47 +22,48 @@ def round_significant(number, significant_figures=6):
     format_string = f"{{:.{significant_figures}g}}"
     return float(format_string.format(number))
 
-def main(env, chain_len, horizon, learning_rates, num_episodes, num_runs, dir_title, plot_directory, is_show_plot=False):
+
+def main(env, chain_len, horizon, learning_rates, num_episodes, num_runs, dir_title, plot_directory,
+         is_show_plot=False, is_save_subopt_over_init_state=True, is_save_prob_optimal_action=False,
+         is_save_subopt_over_init_state_last_iterate=False):
     # Create the loader
-    plot_path = "./exp/" + env + "/" + dir_title +"/" + plot_directory + "/plots/"
+    plot_path = "./exp/" + env + "/" + dir_title + "/" + plot_directory + "/plots/"
     episodes = np.arange(1, num_episodes + 1)
     env_ = ChainEnv(chain_length=chain_len, horizon=horizon)
     num_states = env_.num_states
     initial_learning_rates = len(learning_rates)
-    if initial_learning_rates == 0:
-        learning_rates = np.linspace(-9, 0, num=100)
-        learning_rates = np.exp(learning_rates)
 
-    learning_rates = np.delete(learning_rates, 2)
+    # learning_rates = np.delete(learning_rates, 2)
     temp_learning_rates = []
     for lr in learning_rates:
         temp_learning_rates.append(round_significant(lr))
     learning_rates = np.array(temp_learning_rates)
-    # arr_og = []
-    # for lr in learning_rates:
-    #     arr_og.append(round_significant(lr))
-    #
-    # print(len(arr_og))
-    #
-    # print('===================================')
-    # arr = []
-    # for file in os.listdir("./exp/" + env + "/" + dir_title +"/" + plot_directory + "/models/"):
-    #     if file.startswith("subopt_lr_") and file.endswith(".npy"):
-    #         # Check if the file name contains a learning rate
-    #         if float(file.split('_lr_')[1].split('_run_')[0]) not in arr:
-    #             arr.append(float(file.split('_lr_')[1].split('_run_')[0]))
-    # print(len(sorted(arr)))
-    # exit()
 
     results_by_lr = {lr: {
-        # "suboptimality_histories": np.zeros((num_runs, num_episodes)),
         "suboptimality_over_init_states_histories": np.zeros((num_runs, num_episodes)),
-        # "probability_assigned_to_optimal_actions_histories": np.zeros((num_runs, num_episodes, horizon, num_states)),
     } for lr in learning_rates}
 
+    if is_save_subopt:
+        for lr in learning_rates:
+            results_by_lr[lr]['suboptimality_histories'] = np.zeros((num_runs, num_episodes))
+
+    if is_save_prob_optimal_action:
+        for lr in learning_rates:
+            results_by_lr[lr]['probability_assigned_to_optimal_actions_histories'] = np.zeros(
+                (num_runs, num_episodes, horizon, num_states))
+
     # Load the result
-    # titles = ["subopt_lr_", "subopt_over_init_state_lr_", "prob_to_opt_action_lr_"]
-    titles = ["subopt_over_init_state_lr_"]
+    if is_save_subopt and is_save_prob_optimal_action:
+        titles = ["subopt_lr_", "subopt_over_init_state_lr_", "prob_to_opt_action_lr_"]
+    elif is_save_subopt and not is_save_prob_optimal_action:
+        titles = ["subopt_lr_", "subopt_over_init_state_lr_"]
+    elif not is_save_subopt and is_save_prob_optimal_action:
+        titles = ["subopt_over_init_state_lr_", "prob_to_opt_action_lr_"]
+    elif not is_save_subopt and not is_save_prob_optimal_action:
+        titles = ["subopt_over_init_state_lr_"]
+    elif is_save_subopt_over_init_state_last_iterate:
+        titles = ['subopt_over_init_state_lr_']
+
     for title in titles:
         for lr in learning_rates:
             for n in range(num_runs):
@@ -74,12 +75,12 @@ def main(env, chain_len, horizon, learning_rates, num_episodes, num_runs, dir_ti
                     with open(f"./exp/{env}/{dir_title}/{plot_directory}/models/{title}{lr}_run_{n}.npy", "rb") as f:
                         results = np.load(f)
                 # Store the results in the dictionary
-                # if title == "subopt_lr_":
-                #     results_by_lr[lr]["suboptimality_histories"][n] = results
+                if title == "subopt_lr_":
+                    results_by_lr[lr]["suboptimality_histories"][n] = results
                 if title == "subopt_over_init_state_lr_":
                     results_by_lr[lr]["suboptimality_over_init_states_histories"][n] = results
-                # elif title == "prob_to_opt_action_lr_":
-                #     results_by_lr[lr]["probability_assigned_to_optimal_actions_histories"][n] = results
+                elif title == "prob_to_opt_action_lr_":
+                    results_by_lr[lr]["probability_assigned_to_optimal_actions_histories"][n] = results
             print('lr: ', lr)
 
     # Plot the result
@@ -92,10 +93,19 @@ def main(env, chain_len, horizon, learning_rates, num_episodes, num_runs, dir_ti
         plot_opt_policy(results_by_lr, 1000, learning_rates[:4], plot_path, is_show_plot)
         plot_opt_policy_each_run(num_runs, results_by_lr, 1000, learning_rates[:4], plot_path, is_show_plot)
 
+    if is_save_subopt:
+        plot_supopt_each_run(num_runs, results_by_lr, plot_path, episodes, is_show_plot)
+    if is_save_prob_optimal_action:
+        plot_opt_policy(results_by_lr, 1000, learning_rates[:4], plot_path, is_show_plot)
+        plot_opt_policy_each_run(num_runs, results_by_lr, 1000, learning_rates[:4], plot_path, is_show_plot)
+    if not is_save_subopt_over_init_state_last_iterate:
+        plot_average_subopt(num_runs, results_by_lr, learning_rates, "average_suboptimality", plot_path, episodes,
+                            is_show_plot)
+        # plot_average_subopt(num_runs, results_by_lr, [1e-5, 1e-3, 0.1], "average_suboptimality_specific_lr", plot_path,
+        #                     episodes, is_show_plot)
     else:
-        plot_average_supopt_last_iterate(num_runs, results_by_lr, learning_rates, episodes, "average_suboptimality_last_iterate", plot_path, is_show_plot)
-
-
+        plot_average_supopt_last_iterate(num_runs, results_by_lr, learning_rates, episodes,
+                                         "average_suboptimality_last_iterate", plot_path, is_show_plot)
 
 if __name__ == "__main__":
     with open("./config/chain.yaml", "r") as f:
@@ -105,6 +115,18 @@ if __name__ == "__main__":
     parser.add_argument('--title', type=str, default='default')
     parsed_args = parser.parse_args()
 
+    if len(list(config['learning_rates'])) > 0:
+        learning_rates = list(config['learning_rates'])
+    else:
+        learning_rates = np.linspace(-9, 0, num=50)
+
     main(env=config['env_name'], chain_len=config['chain_len'], horizon=config['horizon'],
-         learning_rates=list(config['learning_rates']), num_episodes=config['num_episodes'], num_runs=config['num_runs'],
-                             dir_title = parsed_args.title, plot_directory=config['plot_directory'], is_show_plot=config['is_show_plot'])
+         learning_rates=list(config['learning_rates']), num_episodes=config['num_episodes'],
+         num_runs=config['num_runs'],
+         dir_title=parsed_args.title, plot_directory=config['plot_directory'], is_show_plot=config['is_show_plot'],
+         is_save_subopt=config['is_save_subopt_histories'],
+         is_save_subopt_over_init_state=config['is_save_subopt_over_init_state_histories'],
+         is_save_prob_optimal_action=config['is_save_prob_optimal_action_histories'],
+         is_save_subopt_over_init_state_last_iterate=config[
+             'is_save_subopt_over_init_state_last_iterate']
+         )
